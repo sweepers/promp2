5a4b9c5916e4b790d3ce0ad3c43c02ffbe74ee68
sweepers <deoz@windowslive.com>
2020-11-28 19:20:45 +0700

    change-meta-title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'PROMPT Corporate Registration',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'ก้าาวแรกสู่ความสำเร็จธุรกิจคุณ.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
