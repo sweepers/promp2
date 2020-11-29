07a8aff0cbc527f2a7184152ddffb2fcf3b25ab8
sweepers <deoz@windowslive.com>
2020-11-29 13:05:51 +0700

    update image

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -19,13 +19,18 @@ export const IndexPageTemplate = ({
    
       <div className="container">
         <div className="row">
-            <div className="col-lg-4">
+            <div className="col-lg-6" style={{
+      backgroundImage: `url(${
+        !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+      })`,
+      
+    }}>
               <h1>ยินดีต้อรับสู่</h1>
               <hr />
               PROMPT Corpate Registration<br />
               ก้าวแรกสู่ความสำเร็จ
             </div>
-            <div className="col-lg-8">
+            <div className="col-lg-6">
                 fsfd
             </div>
         </div>
