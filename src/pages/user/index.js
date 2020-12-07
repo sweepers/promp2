c6236801cd0e0c0638e84d8c249b95a48ebaa817
sweepers <deoz@windowslive.com>
2020-12-07 14:20:27 +0700

    page rigester

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -4,6 +4,8 @@ import Layout from '../../components/Layout'
 import Navbar from '../../components/Navbar'
 import Sitebarleft from '../../components/sidebar-left'
 import Sitebarsubleft from '../../components/sidebarsub-left'
+import Documentsvg from '../../assets/document.svg'
+import { Link } from 'gatsby'
 
 function encode(data) {
   return Object.keys(data)
@@ -15,6 +17,8 @@ export default class Index extends React.Component {
   constructor(props) {
     super(props)
     this.state = { isValidated: false }
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {step: 1};
   }
 
   handleChange = (e) => {
@@ -35,8 +39,20 @@ export default class Index extends React.Component {
       .then(() => navigate(form.getAttribute('action')))
       .catch((error) => alert(error))
   }
+  handleClick(e) {
+   
+    this.setState({ step:'last'})
+  }
 
   render() {
+    
+    let content_right = this.step1();
+    if(this.state.step == 1){
+      
+    }else{
+       content_right = this.steplast();
+    }
+   
     return (
    
       <Layout >
@@ -51,10 +67,9 @@ export default class Index extends React.Component {
                     <div className="bg-white">
                         <div className="row">
                             <Sitebarsubleft />
-                            <div className="col-7 text-center text-middle">
-                                ส่งแบบคำขอจดทะเบียนบริษัท <br />
-                                โปรดรอการตรวจสอบและยืนยันการจองชื่อ
-                            </div>
+                            
+                            { content_right }
+                          
                         </div>
                     </div>
                 </div>
@@ -66,5 +81,35 @@ export default class Index extends React.Component {
         
       </Layout>
     )
+  }
+  step1(){
+    return (
+      <div className="col-7 mt-5 text-grey">
+          <b>แบบฟอร์มกรอกข้อมูล</b> <br />
+          โปรดรอการตรวจสอบและยืนยันการจองชื่อ
+          <div className="border-orange mt-3 mr-3">
+            <div className="row">
+              <div className="col-2 pl-4 align-self-center"><Documentsvg /></div>
+              <div className="col-7 p-2">
+                <h4 className="c-main">แบบฟอร์มการจดทะเบียน
+จัดตั้งบริษัท</h4>
+              </div>
+              <div className="col-3 p-3 align-self-center"><span className="c-main cursor" onClick={this.handleClick}>ตรวจสอบ</span></div>
+              
+            
+            </div>
+          </div>
+          
+      </div>
+    );
+  }
+  steplast(){
+    return (
+      <div className="col-7 text-center align-self-center">
+          ส่งแบบคำขอจดทะเบียนบริษัท <br />
+          กรุณาตรวจสอบความถูกต้องของข้อมูลก่อนส่งแบบคำขอ
+       
+      </div>
+    );
   }
 }
