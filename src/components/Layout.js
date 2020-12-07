894f9e8c30eb6b52af106dc06a085855c873750c
sweepers <deoz@windowslive.com>
2020-12-07 12:43:58 +0700

    lastboot

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,10 +9,10 @@ import '../css/promp.css'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
 //import 'bootstrap/dist/js/bootstrap.min.js'
-import 'jquery/dist/jquery.min'
-import 'popper.js/dist/popper.min'
+//import 'jquery/dist/jquery.min'
+//import 'popper.js/dist/popper.min'
 //import '../js/bootstrap.min'
-import 'bootstrap/dist/js/bootstrap.min.js'
+//import 'bootstrap/dist/js/bootstrap.min.js'
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
   return (
@@ -48,7 +48,9 @@ const TemplateWrapper = ({ children }) => {
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
-
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
